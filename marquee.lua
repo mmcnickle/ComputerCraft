254dd11b7568c6fb29a14dde8791e7049fd01237
Martin McNickle <mmcnickle@gmail.com>
2013-01-10 23:19:38 +0000

    Text actually scrolls

--- marquee.lua
+++ marquee.lua
@@ -1,12 +1,13 @@
 local monitor = peripheral.wrap("right")
 
+-- Initial monitor configuration
+monitor.setTextScale(5)
+
 local text = "Hello, world!"
 
-local position = 1
-
-for i = 1, string.len(text), -1 do
+for i = 1, -string.len(text), -1 do
   monitor.clear()
   monitor.setCursorPos(i, 1)
   monitor.write(text)
-  sleep(1)
+  sleep(0.5)
 end
