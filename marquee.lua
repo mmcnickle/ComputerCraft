0ac427bab9da4d31f3351b7a5e9fdfbb0e5c39bc
Martin McNickle <mmcnickle@gmail.com>
2013-01-10 23:42:37 +0000

    Takes user input as text to display on the board

--- marquee.lua
+++ marquee.lua
@@ -1,5 +1,3 @@
-args = {...}
-
 local monitor = peripheral.wrap("right")
 local delay = 0.5
 
@@ -7,11 +5,15 @@ local delay = 0.5
 monitor.setTextScale(5)
 monitor.setCursorBlink(false)
 
-local text = args[1]
+-- Get text to display
+term.write("Text to display: ")
+local text = read()
 
-for i = 1, -string.len(text) + 1, -1 do
-  monitor.clear()
-  monitor.setCursorPos(i, 1)
-  monitor.write(text .. " " .. text)
-  sleep(delay)
+while true do
+    for i = 1, -string.len(text) + 1, -1 do
+      monitor.clear()
+      monitor.setCursorPos(i, 1)
+      monitor.write(text .. " " .. text)
+      sleep(delay)
+    end
 end
